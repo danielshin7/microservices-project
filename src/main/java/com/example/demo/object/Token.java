c91bfa4d21a61fb4f3ff379ed7a14b0d24c0bb5a
wasadmin <daniel.shin@adp.com>
2024-08-23 19:21:53 +0000

    stage 4

---
+++ src/main/java/com/example/demo/object/Token.java
@@ -0,0 +1,22 @@
+/*
+ * Class to create the token object
+ */
+package com.example.demo.object;
+
+public class Token {
+	String token;
+
+	public Token(String token) {
+		super();
+		this.token = token;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
+}
